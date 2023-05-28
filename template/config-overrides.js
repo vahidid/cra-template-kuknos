8455b4c9ca965a11ac834f964c1c15a7d17358f7
Vahid Hasani <vahidhasani09@gmail.com>
2023-05-28 09:07:35 +0330

    Add alias path and some new lib

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -1,6 +1,21 @@
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const path = require('path');
 
 module.exports = function override(config, env) {
+  // Alias
+  config.resolve.alias = {
+    "@components": path.resolve(__dirname, "./src/Components/"),
+    "@assets": path.resolve(__dirname, "./src/Assets/"),
+    "@hooks": path.resolve(__dirname, "./src/Hooks/"),
+    "@interfaces": path.resolve(__dirname, "./src/Interfaces/"),
+    "@models": path.resolve(__dirname, "./src/Models/"),
+    "@pages": path.resolve(__dirname, "./src/Pages/"),
+    "@routes": path.resolve(__dirname, "./src/Routes/"),
+    "@services": path.resolve(__dirname, "./src/Services/"),
+    "@utils": path.resolve(__dirname, "./src/Utils/"),
+    "@theme": path.resolve(__dirname, "./src/Theme/"),
+  }
+
   config.plugins.push(
     new SentryWebpackPlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
